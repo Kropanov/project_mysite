0a5a2e70d62a91396382b62b9db9bd67706a81a9
Maxim Kropanov <kropanov2012@gmail.com>
2021-12-06 21:34:32 +0300

    Создание меню сайта без ссылочной структуры, проверка работы context, замена логотипа на оригинал из прототипа в Figma.

--- interface/views.py
+++ interface/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "interface/index.html")
+    context = { 
+        "result": [12, 3, 7]
+    }
+    return render(request, "interface/index.html", context)
